b0cb0d0a102fec76e6312fefbd0fbb57f17d97dd
gtabuada <ggtbh@outlook.com>
2024-05-19 22:11:25 -0300

    Colorscheme & telescope improvements

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,12 +2,22 @@ return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	priority = 1000,
-	opts = {},
 	config = function()
 		require("catppuccin").setup({
 			transparent_background = true,
 			integrations = {
+				noice = true,
 				notify = true,
+				fidget = true,
+				harpoon = true,
+				mason = true,
+				mini = {
+					enabled = true,
+					indentscope_color = "", -- catppuccin color (eg. `lavender`) Default: text
+				},
+				telescope = {
+					enabled = true,
+				},
 			},
 		})
 		vim.cmd.colorscheme("catppuccin")
